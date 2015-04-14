70ce4e4f0ce2472a13fcb0876bb4a96fdc1f2694
willricketts <will@willricketts.com>
2015-04-14 18:59:37 -0400

    fixing submission test

--- test/submit.js
+++ test/submit.js
@@ -3,38 +3,39 @@ var assert = require('assert'),
     request = require('request'),
     routes = require('../main');
 
+describe('submission and consumption', function() {
+  describe('Submit Link', function() {
+    var post1 = { payload: 'http://google.com' }
 
-describe('Submit Link', function() {
-  var post1 = { payload: 'http://google.com' }
+    var post2 = { payload: 'google.com' }
 
-  var post2 = { payload: 'google.com' }
+    var post3 = { payload: 'asdfasdfasdf' }
 
-  var post3 = { payload: 'asdfasdfasdf' }
-
-  it('should accept a properly formatted create request with http', function(done) {
-    request.post( 'http://localhost:3000/shorten', { form: post1 }, function(err, res, body) {
-      should.not.exist(err);
-      var response = JSON.parse(res.body);
-      response.payload.should.equal('http://google.com');
-      done();
+    it('should accept a properly formatted create request with http', function(done) {
+      request.post( 'http://localhost:3000/shorten', { form: post1 }, function(err, res, body) {
+        should.not.exist(err);
+        var response = JSON.parse(res.body);
+        response.payload.should.equal('http://google.com');
+        done();
+      });
     });
+
+    it('should accept a properly formatted create request without http', function(done) {
+      request.post( 'http://localhost:3000/shorten', { form: post2 }, function(err, res, body) {
+        should.not.exist(err);
+        var response = JSON.parse(res.body);
+        response.payload.should.equal('http://google.com');
+        done();
+      });
+    });
+
+    it('should reject an improperly formatted URL', function(done) {
+      request.post( 'http://localhost:3000/shorten', { form: post3 }, function(err, res, body) {
+        should.not.exist(err);
+        var response = JSON.parse(res.body);
+        response.error.should.equal('Invalid URL');
+        done();
+      });
+    })
   });
-
-  it('should accept a properly formatted create request without http', function(done) {
-    request.post( 'http://localhost:3000/shorten', { form: post2 }, function(err, res, body) {
-      should.not.exist(err);
-      var response = JSON.parse(res.body);
-      response.payload.should.equal('http://google.com');
-      done();
-    });
-  });
-
-  it('should reject an improperly formatted URL', function(done) {
-    request.post( 'http://localhost:3000/shorten', { form: post3 }, function(err, res, body) {
-      should.not.exist(err);
-      var response = JSON.parse(res.body);
-      response.error.should.equal('Invalid URL');
-      done();
-    });
-  })
 });
