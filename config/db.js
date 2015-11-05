3858d0433bcb0a8c90b9a7555cfe6b0074d69d6b
Will Ricketts <will@willricketts.com>
2015-11-05 15:41:36 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 var mongoUri = require('mongodb-uri');
 
 var dbUser = process.env.db_user,
-    dbPass = process.env.db_pass;
+    dbPass = process.env.db_pass,
+    dbHost = process.env.db_host,
+    db = process.env.db;
 
-var connString = 'mongodb://' + dbUser + ':' + dbPass + '@ds061741.mongolab.com:61741/wayneshorter';
+var connString = 'mongodb://' + dbUser + ':' + dbPass + '@' + dbHost + '/' + db;
 
 var dbUri = mongoUri.formatMongoose(connString);
 
