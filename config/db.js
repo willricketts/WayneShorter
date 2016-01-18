ea3f905386b15e0f9f285cc242b7a75d8e49ae27
Will Ricketts <will@willricketts.com>
2016-01-17 19:50:32 -0500

    fixing old bugs

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 var mongoUri = require('mongodb-uri');
 
-var dbUser = process.env.db_user,
-    dbPass = process.env.db_pass,
-    dbHost = process.env.db_host,
-    db = process.env.db;
+var dbUser = process.env.db_user || '',
+    dbPass = process.env.db_pass || '',
+    dbHost = process.env.db_host || 'localhost',
+    db = process.env.db || 'wayneshorter',
+    connString;
 
-var connString = 'mongodb://' + dbUser + ':' + dbPass + '@' + dbHost + '/' + db;
+if(dbUser && dbPass) {
+  connString = 'mongodb://' + dbUser + dbPass + dbHost + '/' + db;
+}
+else {
+  connString = 'mongodb://' + dbHost + '/' + db;
+}
 
 var dbUri = mongoUri.formatMongoose(connString);
 
